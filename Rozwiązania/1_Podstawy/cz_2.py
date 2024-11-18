0ce2da48ad0d39f013e02addf2343e563a6dd09d
Patryk Palej <patrykpalej@gmail.com>
2024-11-18 13:24:06 +0100

    add changes

---
+++ Rozwiązania/1_Podstawy/cz_2.py
@@ -0,0 +1,16 @@
+text = "Skopiuj treść tego zadania i przypisz do nowo utworzonej zmiennej text." \
+       " Następnie utwórz kilka nowych zmiennych: text_lower, text_upper, text_capitalize " \
+       "oraz text_title, do których przypiszesz odpowiednio przekształconą" \
+       " zmienną text i wyprintuj je."
+
+text_lower = text.lower()
+text_upper = text.upper()
+text_capitalize = text.capitalize()
+text_title = text.title()
+
+half_text = text[:int(len(text)/2)]
+print(half_text)
+
+my_input = input("Input any text: ")
+
+print(f"Powyższy tekst posiada {len(my_input)} znaków")
