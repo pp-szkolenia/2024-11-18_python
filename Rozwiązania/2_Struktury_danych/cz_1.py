45da19ab70ee6486eaed82ee378451062a6c3874
Patryk Palej <patrykpalej@gmail.com>
2024-11-18 14:11:01 +0100

    add changes

---
+++ Rozwiązania/2_Struktury_danych/cz_1.py
@@ -0,0 +1,16 @@
+my_list = [1, 1, 2, 3, 5]
+my_tuple = (2, 4, 6)
+my_set = {"sin", "cos", "tg", "ctg"}
+
+my_dict = {
+    "my_list": my_list,
+    "my_tuple": my_tuple,
+    "my_set": my_set
+}
+
+print(my_dict)
+
+print(type(my_list))
+print(type(my_tuple))
+print(type(my_set))
+print(type(my_dict))
