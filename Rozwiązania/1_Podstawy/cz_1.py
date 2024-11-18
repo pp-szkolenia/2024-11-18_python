0e9a3ec1ebf487299ae2c11b7177349ad3f236db
Patryk Palej <patrykpalej@gmail.com>
2024-11-18 12:03:10 +0100

    add changes

---
+++ Rozwiązania/1_Podstawy/cz_1.py
@@ -0,0 +1,29 @@
+a = 1
+b = 1.23
+c = "hello"
+d = True
+
+print("Zmienna c składa się z", len(c), "znaków")
+
+print(round(b, 1))
+
+print(a, b, c, d)
+
+type_a = type(a)
+type_b = type(b)
+type_c = type(c)
+type_d = type(d)
+
+print(type_a, type_b, type_c, type_d)
+
+x = a / 2
+print(x)
+
+x += 1
+print(x)
+
+x *= 3
+print(x)
+
+x /= 2
+print(x)
