0cc38fe41698a199239fd86647fce498bdeb5d6c
Patryk Palej <patrykpalej@gmail.com>
2024-11-19 09:37:14 +0100

    add changes

---
+++ Rozwiązania/3_Instrukcje_warunkowe/cz_1.py
@@ -0,0 +1,18 @@
+my_list = [3, 9, 4, 2, 8]
+
+print("4" in my_list)
+print(9 not in my_list)
+print(len(my_list) > 4)
+print(my_list[0] != 3)
+print(my_list[-1] >= 8)
+
+number = float(input("Podaj liczbę: "))
+if number > 0:
+    print("Liczba jest dodatnia")
+
+
+a = float(input("Podaj liczbę a:"))
+b = float(input("Podaj liczbę b:"))
+
+if a > 0 > b:
+    print("Warunek jest spełniony")
