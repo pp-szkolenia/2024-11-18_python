000e4b4d11b69effb8617ef07a7ae3ae530f42a2
Patryk Palej <patrykpalej@gmail.com>
2024-11-19 15:10:16 +0100

    add changes

--- Rozwiązania/5_funkcje/cz_2.py
+++ Rozwiązania/5_funkcje/cz_2.py
@@ -1,11 +1,28 @@
+def validate_comment(comment):
+    if not isinstance(comment, dict):
+        return False
+
+    if not all(param in comment for param in ["author", "text"]):
+        return False
+
+    if not (isinstance(comment["text"], str) and
+            isinstance(comment["author"], str)):
+        return False
+
+    return True
+
+
 def add_comment(task, comment):
-    task["comments"].append(comment)
+    if validate_comment(comment):
+        task["comments"].append(comment)
+    else:
+        print("ERROR, niepoprawny komentarz")
 
 
 task = {"is_completed": True,
         "comments": []}
 
-comment = {"author": "...", "text": "..."}
+comment = ["hello"]  # {"author": "...", "text!": "..."}
 
 add_comment(task, comment)
 print(task)
