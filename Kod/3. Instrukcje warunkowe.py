dd851ed7a16fc14066dba3f42e5c247ead8371ae
Patryk Palej <patrykpalej@gmail.com>
2024-11-19 09:47:27 +0100

    add changes

--- Kod/3. Instrukcje warunkowe.py
+++ Kod/3. Instrukcje warunkowe.py
@@ -70,3 +70,46 @@ task_2_priority = 2
 
 if task_1_priority < task_2_priority:
     print("Task 1 is more important")
+
+
+
+list_of_tasks = ["Learn Python", "Do exercises",
+                 "Drink coffee", "Finish my work"]
+
+
+# if len(list_of_tasks) == 0:
+if not list_of_tasks:
+    print("You've completed all your tasks!")
+else:
+    print("There is still something to do")
+
+
+n_of_completed_tasks = 4
+
+is_new_task_finished = False  # False
+
+if is_new_task_finished:
+    n_of_completed_tasks += 1
+else:
+    print("Complete your new task!")
+
+print(f"Number of completed tasks: {n_of_completed_tasks}")
+
+
+status = "fsdsfs"
+
+
+if status == "To do":
+    print("Do the task")
+elif status == "Done":
+    print("The task is done")
+else:
+    print("Unknown status")
+
+
+variable = "hello"
+
+if isinstance(variable, str):
+    if variable[0] == "h":
+        print(True)
+
--- main.py
+++
@@ -1,5 +0,0 @@
-tags_list = ["dev", "python"]
-
-if "python" in tags_list:
-    print("Python is in the tags")
-
