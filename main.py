822a344caec86ed3c787ed1204df18dfb6d18b59
Patryk Palej <patrykpalej@gmail.com>
2024-11-20 10:20:44 +0100

    add changes

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-try:
-    x = 2 / 0
-except ZeroDivisionError:
-    print("Nastąpił błąd")
-finally:
-    print("To się wykona zawsze")
+# raise TypeError("Błąd związany z typem")
+
+
+assert 4 == 4, "Liczby nie są równe"
+
+# print("Asercja poprawna")
+
+
+def add(a, b):
+    return a + b + 1
+
+
+def test_add():
+    assert add(1, 2) == 3, "..."
+
+
+test_add()
