fcb185b6f9f540fc385fcd2db9b8ad339894c63f
Patryk Palej <patrykpalej@gmail.com>
2024-11-20 14:55:18 +0100

    add changes

--- Rozwiązania/8_Biblioteka_os/cz_2.py
+++ Rozwiązania/8_Biblioteka_os/cz_2.py
@@ -7,3 +7,11 @@ os.rmdir("nowy_folder")
 
 os.makedirs("nowy_folder/podfolder")
 shutil.rmtree("nowy_folder")
+
+
+
+for root, dirs, files in os.walk("test_walk"):
+    for file in files:
+        if file.split(".")[-1] == "txt":
+            with open(os.path.join(root, file), "r") as f:
+                print(f.read(), "\n")
--- main_script.py
+++ main_script.py
@@ -5,5 +5,7 @@ from pprint import pprint
 for root, dirs, files in os.walk("test_walk"):
     for file in files:
         if file.split(".")[-1] == "txt":
-            print(file)
+            with open(os.path.join(root, file), "r") as f:
+                print(f.read(), "\n")
+
 
