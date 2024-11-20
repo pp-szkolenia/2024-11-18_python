bd7e2405da4f6323b960fae958b9f5080f376ba0
Patryk Palej <patrykpalej@gmail.com>
2024-11-20 14:12:09 +0100

    add changes

--- main_script.py
+++ main_script.py
@@ -1,20 +1,10 @@
 import os
+import shutil
 
-print(os.getcwd())  # get current working directory
+os.rmdir("new_directory")
 
-print(sorted(os.listdir()))
+os.mknod("test.txt")
 
-print(os.listdir("Kod"))
+os.mkdir("folder/subfolder")
 
-print(os.path.join(os.getcwd(), "Kod"))
-
-print(os.path.isdir("Kod/1. Podstawy.md"))
-
-projects_path = "test_walk_1/projects"
-# print([item
-#        for item in os.listdir(projects_path)
-#        if os.path.isfile(os.path.join(projects_path, item))])
-
-
-print(os.path.exists("functions.py"))
-
+shutil.rmtree("folder")
