f0d1fa63cd351b2852ab10d93607de160f27bc1c
Patryk Palej <patrykpalej@gmail.com>
2024-11-21 13:46:12 +0100

    add changes

--- Kod/12. web scraping.py
+++ Kod/12. web scraping.py
@@ -1,6 +1,16 @@
 import requests
 
 
+url = "http://localhost:8000"
+response = requests.get(url)
+# print(type(response), response.status_code, response.ok)
+
+print(response.json())
+
+
+import requests
+
+
 API_KEY = "bef18344d09a9963fda9d0c8402ace0e"  # wpisz swój klucz API
 DATE = "2019-05-12"
 CURRENCIES_LIST = "EUR,PLN"
