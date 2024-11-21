604d5ebeb546bd1d04e23ca260375e57937d02af
Patryk Palej <patrykpalej@gmail.com>
2024-11-21 13:55:18 +0100

    add changes

--- main_script.py
+++ main_script.py
@@ -1,15 +1,12 @@
 import requests
 
+CITY = "Krakow"
+API_KEY = "7d0c48134ae346811fa50cf99109251f"  # wpisz swój klucz API
 
-API_KEY = "bef18344d09a9963fda9d0c8402ace0e"  # wpisz swój klucz API
-DATE = "2019-05-12"
-CURRENCIES_LIST = "EUR,PLN"
+my_url = f"http://api.openweathermap.org/data/2.5/forecast?q={CITY}&appid={API_KEY}&units=metric"
 
-# my_url = f"http://api.currencylayer.com/historical?access_key={API_KEY}&date={DATE}&currencies={CURRENCIES_LIST}"
-
-url = "http://api.currencylayer.com/historical"
-params = {"access_key": API_KEY, "date": DATE, "currencies": CURRENCIES_LIST}
-response = requests.get(url, params=params)
+response = requests.get(my_url)
 data = response.json()
-# print(response.url)
-print(data)
+
+for item in data["list"]:
+    print(item["dt_txt"], item["main"]["temp"], sep=' | ')
