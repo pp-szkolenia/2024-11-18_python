ac1260bac7f461eefc3b1e49da0f84e8077078ff
Patryk Palej <patrykpalej@gmail.com>
2024-11-21 09:34:04 +0100

    add changes

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+tasks_data = [
+    {"description": "Learn FastAPI", "priority": 2, "is_completed": False},
+    {"description": "Do exercises", "priority": 1, "is_completed": True}
+]
+
 
 @app.get("/")
 def root():
     return {"message": "Hello world!!!"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return {"result": tasks_data}
